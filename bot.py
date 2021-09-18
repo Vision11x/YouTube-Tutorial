de80bd0658fb1709e5a6574e2e295d6b479bd515
Vision11x <88927939+Vision11x@users.noreply.github.com>
2021-09-18 22:45:58 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,37 +7,37 @@ client = commands.Bot(command_prefix = commands.when_mentioned_or('-'), intents 
 @client.event
 async def on_ready():
     print("Ready")
-
+    
 @client.command()
 async def load(ctx, extension):
-    vision_id = #your id
-    if ctx.author == vision_id:
-        client.load_extension(f"{extension}")
-        await ctx.send(f"Loaded {extension}")
+    vision_id = 880846666624811088
+    if ctx.author.id == vision_id:
+        client.load_extension(f'cogs.{extension}')
+        await ctx.send(f"Loaded {extension} Successfully")
     else:
         return
 
-@client.command()
+@client.command()   
 async def unload(ctx, extension):
-    vision_id = #your id
-    if ctx.author == vision_id:
-        client.unload_extension(f"{extension}")
-        await ctx.send(f"unloaded {extension}")
+    vision_id = 880846666624811088
+    if ctx.author.id == vision_id:
+        client.unload_extension(f'cogs.{extension}')    
+        await ctx.send(f"Unloaded {extension} Successfully")
     else:
         return
 
 @client.command()
 async def reload(ctx, extension):
-    vision_id = #your id
-    if ctx.author == vision_id:
-        client.unload_extension(f"{extension}")
-        client.load_extension(f"{extension}")
-        await ctx.send(f"reloaded {extension}")
+    vision_id = 880846666624811088
+    if ctx.author.id == vision_id:
+        client.unload_extension(f'cogs.{extension}')
+        client.load_extension(f'cogs.{extension}')
+        await ctx.send(f"Reloaded {extension} Successfully")
     else:
         return
 
-for filename in os.listdir("./cogs"):
-    if filename.endswith(".py"):
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
 client.run("your token")
